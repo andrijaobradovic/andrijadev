c48cbf6b08cc7d5c2b882f1eea9f6b797641bbad
Andrija Obradovic <andrija.kl.obradovic@gmail.com>
2026-06-24 15:06:25 +0200

    Cetvrti pokusaj: fix na skrol

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Viewport } from "next";
 import { Geist, Geist_Mono, Raleway, Space_Grotesk } from "next/font/google";
 import { getLocale } from "next-intl/server";
-import { ViewportHeightSync } from "@/components/layout/viewport-height-sync";
 import { cn } from "@/lib/utils";
 import "./globals.css";
 
@@ -50,10 +49,7 @@ export default async function RootLayout({
         spaceGroteskHeading.variable
       )}
     >
-      <body className="flex min-h-lvh flex-col">
-        <ViewportHeightSync />
-        {children}
-      </body>
+      <body className="flex min-h-lvh flex-col">{children}</body>
     </html>
   );
 }
